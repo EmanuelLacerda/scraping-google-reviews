e9718486ce9221654358bb9860dd820b6d8662fa
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2024-10-15 19:54:49 -0300

    chore(server/api): Add general urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
+from django.urls import path, include
+from business.urls import router
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('auth/', include('rest_framework.urls'))
 ]
